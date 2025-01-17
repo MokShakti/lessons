9ba286edf86b83d608e116baebaf72f5ff89edd1
MokShakti <167544238+MokShakti@users.noreply.github.com>
2025-01-17 21:51:57 +0100

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -6,3 +6,24 @@ document.addEventListener("click", function (e) {
 		e.preventDefault();
 	}
 })
+// ---------VIDEO---------
+// пошук кнопки
+const playButton = document.querySelector("#play");
+// перевірка на наявність кнопки
+if (playButton) {
+	// відстежування кліку по кнопці
+	playButton.addEventListener("click", () => {
+		// пошук елемента з id #video
+		const video = document.querySelector("#video");
+		// керування відтворенням відео
+		if (video.paused) {
+			video.play();
+		} else {
+			video.pause();
+		}
+	});
+}
+// якщо кнопки нема вивід в консоль повідомлення
+else {
+	console.error("Елемент #play не знайдено на сторінці.");
+}
